2f493ead0c0244a252fde7b332f7e30ee8966594
陈哲俊 <huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comzhuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comjhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2016-05-09 11:19:59 +0800

    同步更新goods模块

---
+++ modules/admin/goods/brand_module.class.php
@@ -0,0 +1,47 @@
+<?php
+defined('IN_ECJIA') or exit('No permission resources.');
+/**
+ * 品牌列表
+ * @author royalwang
+ *
+ */
+class brand_module implements ecjia_interface {
+	
+	public function run(ecjia_api & $api) {
+		
+		$ecjia = RC_Loader::load_app_class('api_admin', 'api');
+    	$ecjia->authadminSession();
+    	
+		$size = EM_Api::$pagination['count'];
+		$page = EM_Api::$pagination['page'];
+		$options = array(
+			'keywords'	=> _POST('keywords'),
+			'size'		=> $size,
+			'page'		=> $page
+		);
+		$result = RC_Api::api('goods', 'goods_brand_list', $options);
+		$brand = array();
+		if (!empty($result['brand'])) {
+			foreach ($result['brand'] as $val) {
+				$brand[] = array(
+					'brand_id'		=> $val['brand_id'],
+					'brand_name'	=> $val['brand_name'],
+					'brand_logo'	=> !empty($val['brand_logo']) ? RC_Upload::upload_url($val['brand_logo']) : '',
+					'is_show'		=> $val['is_show'],
+				);
+			}
+		}
+		
+        $pager = array(
+				"total" => $result['page']->total_records,
+				"count" => $result['page']->total_records,
+				"more" 	=> $result['page']->total_pages <= $page ? 0 : 1,
+		);
+		
+		EM_Api::outPut($brand, $pager);
+		
+	}
+}
+
+
+// end
