a782e73201fb5d7e17085cc9e201a3a4eb5c19f1
周瑞丽 <huangyuyuan@ecmoban.comzhuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comrhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2016-07-26 10:59:26 +0800

    o2o商家商品分类接口修改

--- modules/admin/goods/category_module.class.php
+++ modules/admin/goods/category_module.class.php
@@ -1,8 +1,8 @@
 <?php
 defined('IN_ECJIA') or exit('No permission resources.');
 /**
- * 单个商品的信息
- * @author luchongchong
+ * 店铺商品分类
+ * @author zrl
  *
  */
 class category_module implements ecjia_interface
@@ -13,9 +13,10 @@ class category_module implements ecjia_interface
     	$ecjia = RC_Loader::load_app_class('api_admin', 'api');
     	$ecjia->authadminSession();
     
-    	RC_Loader::load_app_func('category','goods');
-    	
-    	$data=cat_list(0,0,false);
+    	//RC_Loader::load_app_func('category','goods');
+    	//$data=cat_list(0,0,false);
+    	$seller_id = empty($_SESSION['seller_id']) ? 0 : $_SESSION['seller_id'];
+    	$data = RC_Api::api('goods', 'seller_goods_category', array('type' => 'seller_goods_cat_list_api', 'seller_id' => $seller_id));
       	 foreach ($data as $key=>$value)
       	 {
       	    $outdata[]=array(
