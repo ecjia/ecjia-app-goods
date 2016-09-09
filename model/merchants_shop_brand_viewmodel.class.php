1d7f3f678d54061f1fb1f4bcddd18d3b0efcb424
吴体方 <huangyuyuan@ecmoban.comwhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comthuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comfhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2016-09-09 13:20:30 +0800

    修改优惠活动数据库操作方法

--- model/merchants_shop_brand_viewmodel.class.php
+++ model/merchants_shop_brand_viewmodel.class.php
@@ -11,16 +11,16 @@ class merchants_shop_brand_viewmodel extends Component_Model_View {
 		$this->table_alias_name = 'mb';
 		
 		$this->view = array(
-// 				'merchants_shop_information' => array(
-// 						'type' 	=> Component_Model_View::TYPE_LEFT_JOIN,
-// 						'alias' => 'ms',
-// 						'on' 	=> 'mb.user_id = ms.user_id'
-// 				),
-				'seller_shopinfo' => array(
-						'type' 	=> Component_Model_View::TYPE_LEFT_JOIN,
-						'alias' => 'ssi',
-						'on' 	=> 'ssi.id = mb.seller_id'
-				),
+// 			'merchants_shop_information' => array(
+// 				'type' 	=> Component_Model_View::TYPE_LEFT_JOIN,
+// 				'alias' => 'ms',
+// 				'on' 	=> 'mb.user_id = ms.user_id'
+// 			),
+			'seller_shopinfo' => array(
+				'type' 	=> Component_Model_View::TYPE_LEFT_JOIN,
+				'alias' => 'ssi',
+				'on' 	=> 'ssi.id = mb.seller_id'
+			),
 		);
 		
 		parent::__construct();
