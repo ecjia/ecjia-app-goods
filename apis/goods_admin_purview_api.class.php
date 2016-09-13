e53fe2acddf2e69e7633c79aab254a9e6123da27
宋倩倩 <huangyuyuan@ecmoban.comshuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comqhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comqhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2016-09-13 16:38:39 +0800

    更新商品的权限以及遗漏的语言包

--- apis/goods_admin_purview_api.class.php
+++ apis/goods_admin_purview_api.class.php
@@ -9,26 +9,27 @@ class goods_admin_purview_api extends Component_Event_Api {
     
     public function call(&$options) {
         $purviews = array(
-            array('action_name' => __('商品管理'), 'action_code' => 'goods_manage', 'relevance'   => ''),
-        	array('action_name' => __('更新商品'), 'action_code' => 'goods_update', 'relevance'   => ''),
-            array('action_name' => __('商品回收/恢复'), 'action_code' => 'remove_back', 'relevance'   => ''),
-        	array('action_name' => __('商品彻底删除'), 'action_code' => 'goods_delete', 'relevance'   => ''),
+            array('action_name' => RC_Lang::get('goods::goods.goods_manage'), 'action_code' => 'goods_manage', 'relevance'   => ''),
+        	array('action_name' => RC_Lang::get('goods::goods.goods_update'), 'action_code' => 'goods_update', 'relevance'   => ''),
+            array('action_name' => RC_Lang::get('goods::goods.remove_back'),  'action_code' => 'remove_back', 'relevance'   => ''),
+        	array('action_name' => RC_Lang::get('goods::goods.goods_delete'),'action_code' => 'goods_delete', 'relevance'   => ''),
         		
-            array('action_name' => __('分类管理'), 'action_code' => 'cat_manage', 'relevance'   => ''),
-        	array('action_name' => __('更新分类'), 'action_code' => 'cat_update', 'relevance'   => ''),
-            array('action_name' => __('分类转移/删除'), 'action_code' => 'cat_drop', 'relevance'   => ''),
+            array('action_name' => RC_Lang::get('goods::goods.category_manage'), 'action_code' => 'category_update', 'relevance'   => ''),
+        	array('action_name' => RC_Lang::get('goods::goods.category_update'), 'action_code' => 'category_update', 'relevance'   => ''),
+            array('action_name' => RC_Lang::get('goods::goods.category_move'), 'action_code' => 'category_move', 'relevance'   => ''),
+        	array('action_name' => RC_Lang::get('goods::goods.category_delete'), 'action_code' => 'category_delete', 'relevance'   => ''),
         		
-            array('action_name' => __('商品属性管理'), 'action_code' => 'attr_manage', 'relevance'   => ''),
-        	array('action_name' => __('商品属性更新'), 'action_code' => 'attr_update', 'relevance'   => ''),
-        	array('action_name' => __('商品属性删除'), 'action_code' => 'attr_delete', 'relevance'   => ''),
+            array('action_name' => RC_Lang::get('goods::goods.attr_manage'), 'action_code' => 'attr_manage', 'relevance'   => ''),
+        	array('action_name' => RC_Lang::get('goods::goods.attr_update'), 'action_code' => 'attr_update', 'relevance'   => ''),
+        	array('action_name' => RC_Lang::get('goods::goods.attr_delete'), 'action_code' => 'attr_delete', 'relevance'   => ''),
         		
-        	array('action_name' => __('商品品牌管理'), 'action_code' => 'brand_manage', 'relevance'   => ''),
-        	array('action_name' => __('商品品牌更新'), 'action_code' => 'brand_update', 'relevance'   => ''),
-        	array('action_name' => __('商品品牌删除'), 'action_code' => 'brand_delete', 'relevance'   => ''),
+        	array('action_name' => RC_Lang::get('goods::goods.brand_manage'), 'action_code' => 'brand_manage', 'relevance'   => ''),
+        	array('action_name' => RC_Lang::get('goods::goods.brand_update'), 'action_code' => 'brand_update', 'relevance'   => ''),
+        	array('action_name' => RC_Lang::get('goods::goods.brand_delete'), 'action_code' => 'brand_delete', 'relevance'   => ''),
         		
-            array('action_name' => __('商品类型管理'), 'action_code' => 'goods_type', 'relevance'   => ''),
-        	array('action_name' => __('商品类型更新'), 'action_code' => 'goods_type_update', 'relevance'   => ''),
-        	array('action_name' => __('商品类型删除'), 'action_code' => 'goods_type_delete', 'relevance'   => ''),
+            array('action_name' => RC_Lang::get('goods::goods.goods_type'), 	   'action_code' => 'goods_type', 'relevance'   => ''),
+        	array('action_name' => RC_Lang::get('goods::goods.goods_type_update'), 'action_code' => 'goods_type_update', 'relevance'   => ''),
+        	array('action_name' => RC_Lang::get('goods::goods.goods_type_delete'), 'action_code' => 'goods_type_delete', 'relevance'   => ''),
         );
         return $purviews;
     }
