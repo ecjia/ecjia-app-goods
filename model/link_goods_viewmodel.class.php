368641ab29ce31dbddce65e151c42c33a698ccd9
黄玉园 <huangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2016-09-22 20:36:13 +0800

    app下model修改

--- model/link_goods_viewmodel.class.php
+++ model/link_goods_viewmodel.class.php
@@ -5,8 +5,6 @@ class link_goods_viewmodel extends Component_Model_View {
 	public $table_name = '';
 	public $view = array();
 	public function __construct() {
-		$this->db_config = RC_Config::load_config('database');
-		$this->db_setting = 'default';
 		$this->table_name = 'link_goods';
 		$this->table_alias_name = 'lg';
 		
