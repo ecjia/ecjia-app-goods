5f738252972af4737145ce028518e0dabd080531
季鸿飞 <huangyuyuan@ecmoban.comjhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comfhuangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2016-10-20 15:39:49 +0800

    修改货号搜索商品

--- modules/admin/goods/product_search_module.class.php
+++ modules/admin/goods/product_search_module.class.php
@@ -7,24 +7,24 @@ defined('IN_ECJIA') or exit('No permission resources.');
  */
 class product_search_module extends api_admin implements api_interface {
     public function handleRequest(\Royalcms\Component\HttpKernel\Request $request) {
-    		
+
 		$this->authadminSession();
-		if ($_SESSION['admin_id'] <= 0) {
+		if ($_SESSION['admin_id'] <= 0 && $_SESSION['staff_id'] <= 0) {
 			return new ecjia_error(100, 'Invalid session');
 		}
-		
+
     	$goods_sn	= $this->requestData('goods_sn','');
     	if (empty($goods_sn)) {
     		return new ecjia_error('invalid_parameter', RC_Lang::get('system::system.invalid_parameter'));
     	}
     	$size = $this->requestData('pagination.count', 15);
     	$page = $this->requestData('pagination.page', 1);
-    	
+
 		$device		= $this->requestData['device'];
     	$device_code = $device['code'];
 
 		$db_goods = RC_Model::model('goods/goods_viewmodel');
-    	 	
+
 		$db_goods->view = array(
 			'products' => array(
 					'type'		=> Component_Model_View::TYPE_LEFT_JOIN,
@@ -32,17 +32,20 @@ class product_search_module extends api_admin implements api_interface {
 					'on'		=> 'g.goods_sn=p.product_sn'
 			)
 		);
-		
+
 		$field = 'g.goods_id, g.goods_name, g.shop_price, g.shop_price, g.goods_sn, g.goods_img, g.original_img, g.goods_thumb, p.goods_attr, p.product_sn';
 
     	$where[] = "(goods_sn like '%".$goods_sn."%' OR  product_sn like '%".$goods_sn."%')";
+        if(!empty($_SESSION['store_id'])){
+            $where['store_id'] = $_SESSION['store_id'];
+        }
     	if ($device_code == '8001') {
     		$where = array_merge($where, array('is_delete' => 0, 'is_on_sale' => 1, 'is_alone_sale' => 1));
     		if (ecjia::config('review_goods')) {
     			$where['review_status'] = array('gt' => 2);
     		}
     	}
-    	
+
     	$arr = $db_goods->field($field)->join(array('products'))->where($where)->select();
     	$product_search = array();
 		if (!empty($arr)) {
