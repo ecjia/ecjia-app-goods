3ea60e21dc5eeffb919ccbb15021d3c8ac4ad28c
季鸿飞 <huangyuyuan@ecmoban.comjhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comfhuangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2016-10-20 13:04:35 +0800

    修改编辑商品接口

--- modules/admin/goods/togglesale_module.class.php
+++ modules/admin/goods/togglesale_module.class.php
@@ -7,7 +7,7 @@ defined('IN_ECJIA') or exit('No permission resources.');
  */
 class togglesale_module extends api_admin implements api_interface {
     public function handleRequest(\Royalcms\Component\HttpKernel\Request $request) {
-    		
+
 		$this->authadminSession();
 		if ($_SESSION['admin_id'] <= 0) {
 			return new ecjia_error(100, 'Invalid session');
@@ -15,7 +15,7 @@ class togglesale_module extends api_admin implements api_interface {
 		if (!$this->admin_priv('goods_manage')) {
 			return new ecjia_error('privilege_error', '对不起，您没有执行此项操作的权限！');
 		}
-		
+
 		RC_Loader::load_app_func('global', 'goods');
 		$id = $this->requestData('id');
 		$type = $this->requestData('type');//online 上架;offline下架
@@ -23,19 +23,19 @@ class togglesale_module extends api_admin implements api_interface {
 			return new ecjia_error('invalid_parameter', RC_Lang::get('system::system.invalid_parameter'));
 		}
 		$on_sale = $type == 'online' ? 1 : 0;
-	
+
 		$data = array(
 			'is_on_sale' => $on_sale,
 			'last_update' => RC_Time::gmtime()
 		);
 		$db_goods = RC_Model::model('goods/goods_model');
-		
+
 		$where = array('goods_id' => $id);
 		if ($_SESSION['ru_id'] > 0) {
 			$where = array_merge($where, array('user_id' => $_SESSION['ru_id']));
 		}
 		$db_goods->where($where)->update($data);
-		
+
 		$goods_name = $db_goods->where(array('goods_id' => $id))->get_field('goods_name');
 		if($on_sale == '1') {
 			ecjia_admin::admin_log('上架商品，'.$goods_name, 'setup', 'goods');
--- modules/admin/goods/updateprice_module.class.php
+++ modules/admin/goods/updateprice_module.class.php
@@ -8,7 +8,7 @@ defined('IN_ECJIA') or exit('No permission resources.');
 class updateprice_module extends api_admin implements api_interface {
 	public function handleRequest(\Royalcms\Component\HttpKernel\Request $request) {
     	$this->authadminSession();
-    	
+
 		//请求参数：
        	$goods_id				= $this->requestData('goods_id', 0);
     	if (empty($goods_id)) {
@@ -22,13 +22,13 @@ class updateprice_module extends api_admin implements api_interface {
     	$give_integral			= $this->requestData('give_integral', 0);
     	$rank_integral			= $this->requestData('rank_integral', 0);
     	$integral				= $this->requestData('integral', 0);
-    	
+
     	//促销信息
     	$promote_price			= $this->requestData('promote_price');
     	$is_promote 			= empty($promote_price) ? 0 : 1;
     	$promote_price      	= !empty($promote_price) ?  $promote_price : 0;
     	$promote_start_date		= $this->requestData('promote_start_date');
-    	
+
     	$promote_end_date  		= $this->requestData('promote_end_date');
     	if (($promote_start_date == $promote_end_date) && !empty($promote_start_date) && !empty($promote_end_date)) {
     		$promote_start_date .= ' 00:00:00';
@@ -36,14 +36,14 @@ class updateprice_module extends api_admin implements api_interface {
     	}
     	$promote_start_date     = ($is_promote && !empty($promote_start_date)) ? RC_Time::local_strtotime($promote_start_date) : '';
     	$promote_end_date      	= ($is_promote && !empty($promote_end_date)) ? RC_Time::local_strtotime($promote_end_date) : '';
-    	
+
 	 	//优惠价格、等级价格
     	$volume_number_list 	= $this->requestData('volume_number');
-    	$user_rank_list			= $this->requestData['user_rank'];
-    	
+    	$user_rank_list			= $this->requestData('user_rank');
+
     	$db_goods = RC_Model::model('goods/goods_model');
-    	
-    	RC_Loader::load_app_func('system_goods', 'goods'); 
+
+    	RC_Loader::load_app_func('system_goods', 'goods');
     	RC_Loader::load_app_func('functions', 'goods');
 
     	$volume_number = array();
@@ -52,7 +52,7 @@ class updateprice_module extends api_admin implements api_interface {
     		$volume_number[] = $value['number'];
     		$volume_price[] = $value['price'];
     	}
-    	
+
 		$user_rank  = array();
 		$userprice  = array();
 		foreach ($user_rank_list as $key=>$value){
@@ -63,11 +63,11 @@ class updateprice_module extends api_admin implements api_interface {
 		if (!empty($user_rank) && !empty($userprice)) {
 			handle_member_price($goods_id, $user_rank, $userprice);
 		}
-		
+
 		if (!empty($volume_number) && !empty($volume_price)) {
 			handle_volume_price($goods_id, $volume_number, $volume_price);
 		}
-		
+
     	$data = array(
     			'shop_price'			=> $shop_price,
     			'market_price'			=> $market_price,
