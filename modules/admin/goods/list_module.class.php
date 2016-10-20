025001875bc166a80451b26e86e6f9011b9fc258
季鸿飞 <huangyuyuan@ecmoban.comjhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comfhuangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2016-10-20 09:54:32 +0800

    修复商品列表接口

--- modules/admin/goods/list_module.class.php
+++ modules/admin/goods/list_module.class.php
@@ -7,24 +7,25 @@ defined('IN_ECJIA') or exit('No permission resources.');
  */
 class list_module extends api_admin implements api_interface {
     public function handleRequest(\Royalcms\Component\HttpKernel\Request $request) {
-    		
+
 		$this->authadminSession();
+        _dump($_SESSION['admin_id'],1);
 		if ($_SESSION['admin_id'] <= 0) {
 			return new ecjia_error(100, 'Invalid session');
 		}
 		if (!$this->admin_priv('goods_manage')) {
 			return new ecjia_error('privilege_error', '对不起，您没有执行此项操作的权限！');
 		}
-		
+
 		$on_sale	= $this->requestData('on_sale');//true.在售, false.下架
 		$stock		= $this->requestData('stock');//是否售罄 。true.有货 , false.售罄
 		$sort		= $this->requestData('sort_by', 'sort_order');//默认: sort_order  其他: price_desc, price_asc, stock, click_asc, clcik_desc
 		$keywords	= $this->requestData('keywords', '');
 		$category_id = $this->requestData('category_id', 0);
-		
+
 		$size = $this->requestData('pagination.count', 15);
 		$page = $this->requestData('pagination.page', 1);
-		
+
 		$sort_by = '';
 		/* 推荐类型 */
 		switch ($sort) {
@@ -52,13 +53,13 @@ class list_module extends api_admin implements api_interface {
 			'is_delete' => 0,
 		);
 
-		if ($_SESSION['seller_id'] > 0) {
-			$where = array_merge($where, array('seller_id' => $_SESSION['seller_id']));
+		if ($_SESSION['store_id'] > 0) {
+			$where = array_merge($where, array('store_id' => $_SESSION['store_id']));
 		}
 		if (!empty($on_sale)) {
 			$where['is_on_sale'] = $on_sale == 'true' ? 1 : 0 ;
 		}
-		
+
 		if ($stock == 'false') {
 			$where['goods_number'] = 0;
 		}
@@ -69,19 +70,19 @@ class list_module extends api_admin implements api_interface {
 			$where[] = "(".$children ." OR ".get_extension_goods($children).")";
 		}
 		if ( !empty($keywords)) {
-			$where[] = "( goods_name like '%".$keywords."%' or goods_sn like '%".$keywords."%' )"; 
+			$where[] = "( goods_name like '%".$keywords."%' or goods_sn like '%".$keywords."%' )";
 		}
 		$db = RC_Model::model('goods/goods_viewmodel');
-		
+
 		/* 获取记录条数 */
 		$record_count = $db->join(null)->where($where)->count();
-			
+
 		//实例化分页
 		$page_row = new ecjia_page($record_count, $size, 6, '', $page);
-		
+
 		$field = "goods_id, goods_sn, goods_name, goods_number, shop_price, market_price, promote_price, promote_start_date, promote_end_date, click_count, goods_thumb, is_best, is_new, is_hot, is_shipping, goods_img, original_img, last_update";
 		$data = $db->join(null)->field($field)->where($where)->order($sort_by)->limit($page_row->limit())->select();
-		
+
 		$goods_list = array();
 		if (!empty($data)) {
 			RC_Loader::load_app_func('goods', 'goods');
@@ -117,7 +118,7 @@ class list_module extends api_admin implements api_interface {
 				);
 			}
 		}
-		
+
 		$pager = array(
 				'total' => $page_row->total_records,
 				'count' => $page_row->total_records,
