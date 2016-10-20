098f15be63a3da20b1acb7f676332e4734f19f8c
季鸿飞 <huangyuyuan@ecmoban.comjhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comfhuangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2016-10-20 15:36:09 +0800

    修改商品回收站接口

--- modules/admin/goods/trash_module.class.php
+++ modules/admin/goods/trash_module.class.php
@@ -7,7 +7,7 @@ defined('IN_ECJIA') or exit('No permission resources.');
  */
 class trash_module extends api_admin implements api_interface {
     public function handleRequest(\Royalcms\Component\HttpKernel\Request $request) {
-    		
+
 		$this->authadminSession();
 		if ($_SESSION['admin_id'] <= 0) {
 			return new ecjia_error(100, 'Invalid session');
@@ -15,7 +15,7 @@ class trash_module extends api_admin implements api_interface {
 		if (!$this->admin_priv('remove_back')) {
 			return new ecjia_error('privilege_error', '对不起，您没有执行此项操作的权限！');
 		}
-		
+
 		$id = $this->requestData('id');
 		if (empty($id)) {
 			return new ecjia_error('not_exists_info', '不存在的信息');
@@ -25,13 +25,13 @@ class trash_module extends api_admin implements api_interface {
 				'last_update' => RC_Time::gmtime()
 		);
 		$db_goods = RC_Model::model('goods/goods_model');
-		
+
 		$where = array('goods_id' => $id);
-		if ($_SESSION['ru_id'] > 0) {
-			$where = array_merge($where, array('user_id' => $_SESSION['ru_id']));
+		if ($_SESSION['store_id'] > 0) {
+			$where = array_merge($where, array('store_id' => $_SESSION['store_id']));
 		}
 		$db_goods->where($where)->update($data);
-		
+
 		$goods_name = $db_goods->where(array('goods_id' => $id))->get_field('goods_name');
 		ecjia_admin::admin_log(addslashes($goods_name), 'trash', 'goods'); // 记录日志
 		return array();
