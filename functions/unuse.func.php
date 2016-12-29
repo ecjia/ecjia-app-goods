e93f60cf65ff0cc4ddcc106191de87ba79a7a98b
吴体方 <huangyuyuan@ecmoban.comwhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comthuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comfhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2016-12-29 11:33:26 +0800

    修改购物车判断

--- functions/unuse.func.php
+++ functions/unuse.func.php
@@ -273,4 +273,110 @@ function get_documentTitle_insert_update($dt_list, $cat_id, $dt_id = array()) {
 	}
 }
 
+/**
+ * 查询仓库列表
+ */
+function get_warehouse_goods_list($goods_id = 0) {
+	$db_warehouse_goods = RC_Model::model('goods/warehouser_goods_viewmodel');
+	$rs = $db_warehouse_goods->field('wg.w_id, wg.region_id, wg.region_number, wg.warehouse_price, wg.warehouse_promote_price, rw.region_name')->where(array('goods_id' =>$goods_id))->select();
+	return $rs;
+}
+
+/**
+ * 查询地区仓库列表
+ */
+function get_warehouse_area_goods_list($goods_id = 0) {
+	$db_area_goods = RC_Model::model('goods/warehouse_area_goods_viewmodel');
+	$db_region = RC_Model::model('goods/region_warehouose_model');
+
+	$data = $db_region->where(array('parent_id' => 0))->select();
+	foreach ($data as $key => $val){
+		$arr[$val['region_id']] = $val['region_name'];
+	}
+	$field = 'wa.a_id, wa.region_id, wa.region_number, wa.region_price, wa.region_promote_price, rw.region_name, rw.parent_id';
+	$rs = $db_area_goods->field($field)->where(array('wa.goods_id' => $goods_id))->select();
+	if (!empty($rs)) {
+		foreach ($rs as $key => $val){
+			$rs[$key][ware_name] = $arr[$val['parent_id']];
+			$rs[$key][warehouse] = $val['parent_id'];
+		}
+	}
+	return $rs;
+}
+
+/**
+ * 获得商品的属性和规格
+ *
+ * @access public
+ * @param integer $goods_id
+ * @return array
+ */
+function warehouse_get_goods_properties($goods_id) {
+	$db_good_type = RC_Model::model('goods/goods_type_viewmodel');
+	$db_good_attr = RC_Model::model('goods/goods_attr_viewmodel');
+	$db_goods = RC_Model::model('goods/goods_model');
+	/* 对属性进行重新排序和分组 */
+
+	$db_good_type->view = array (
+			'goods' => array (
+					'type' 	=> Component_Model_View::TYPE_LEFT_JOIN,
+					'alias' => 'g',
+					'field' => 'attr_group',
+					'on' 	=> 'gt.cat_id = g.goods_type'
+			)
+	);
+
+	$grp = $db_good_type->find ( array (
+			'g.goods_id' => $goods_id
+	) );
+	$grp = $grp ['attr_group'];
+	if (! empty ( $grp )) {
+		$groups = explode ( "\n", strtr ( $grp, "\r", '' ) );
+	}
+
+	/* 获得商品的规格 */
+	$db_good_attr->view = array (
+			'attribute' => array (
+					'type'     => Component_Model_View::TYPE_LEFT_JOIN,
+					'alias'    => 'a',
+					'field'    => 'a.attr_id, a.attr_name, a.attr_group, a.is_linked, a.attr_type, ga.goods_attr_id, ga.attr_value, ga.attr_price',
+					'on'       => 'a.attr_id = ga.attr_id'
+			)
+	);
+
+	$res = $db_good_attr->where(array('ga.goods_id' => $goods_id))->order(array('a.sort_order' => 'asc','ga.attr_price' => 'asc','ga.goods_attr_id' => 'asc'))->select();
+	$arr ['pro'] = array (); // 属性
+	$arr ['spe'] = array (); // 规格
+	$arr ['lnk'] = array (); // 关联的属性
+
+	if (! empty ( $res )) {
+		foreach ( $res as $row ) {
+			$row ['attr_value'] = str_replace ( "\n", '<br />', $row ['attr_value'] );
+
+			if ($row ['attr_type'] == 0) {
+				$group = (isset ( $groups [$row ['attr_group']] )) ? $groups [$row ['attr_group']] : RC_Lang::lang ( 'goods_attr' );
+
+				$arr ['pro'] [$group] [$row ['attr_id']] ['name'] = $row ['attr_name'];
+				$arr ['pro'] [$group] [$row ['attr_id']] ['value'] = $row ['attr_value'];
+			} else {
+				$arr ['spe'] [$row ['attr_id']] ['attr_type'] = $row ['attr_type'];
+				$arr ['spe'] [$row ['attr_id']] ['name'] = $row ['attr_name'];
+				$arr ['spe'] [$row ['attr_id']] ['values'] [] = array (
+						'label' => $row ['attr_value'],
+						'price' => $row ['attr_price'],
+						'format_price' => price_format ( abs ( $row ['attr_price'] ), false ),
+						'id' => $row ['goods_attr_id']
+				);
+			}
+
+			if ($row ['is_linked'] == 1) {
+				/* 如果该属性需要关联，先保存下来 */
+				$arr ['lnk'] [$row ['attr_id']] ['name'] = $row ['attr_name'];
+				$arr ['lnk'] [$row ['attr_id']] ['value'] = $row ['attr_value'];
+			}
+		}
+	}
+	return $arr;
+}
+
 // ======================================== 废弃方法整理 end ======================================== //
