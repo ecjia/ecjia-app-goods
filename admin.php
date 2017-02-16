069addf6b94703939ea0276406fbbf20c5748659
王正东 <huangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comxhuangyuyuan@ecmoban.comrhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comghuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2017-02-16 10:38:39 +0800

    修复平台后台商品列表预览跳转至H5商品页

--- admin.php
+++ admin.php
@@ -192,6 +192,13 @@ class admin extends ecjia_admin {
 	public function preview() {
 		$this->admin_priv('goods_manage', ecjia::MSGTYPE_JSON);
 		
+		$goods_id = !empty($_GET['id']) ? intval($_GET['id']) : 0;
+		if (empty($goods_id)) {
+		    return $this->showmessage(RC_Lang::get('goods::goods.no_goods'), ecjia::MSGTYPE_HTML | ecjia::MSGSTAT_ERROR, array('links' => array(array('text' => RC_Lang::get('goods::goods.return_last_page'), 'href' => 'javascript:history.go(-1)'))));
+		}
+		
+		RC_Hook::do_action('goods_admin_priview_handler', $goods_id);
+		
 		ecjia_screen::get_current_screen()->add_nav_here(new admin_nav_here(RC_Lang::get('goods::goods.goods_preview')));
 		ecjia_screen::get_current_screen()->add_help_tab(array(
 			'id'		=> 'overview',
@@ -203,14 +210,12 @@ class admin extends ecjia_admin {
 			'<p><strong>' . RC_Lang::get('goods::goods.more_info') . '</strong></p>' .
 			'<p>' . __('<a href="https://ecjia.com/wiki/帮助:ECJia智能后台:商品列表#.E9.A2.84.E8.A7.88.E5.95.86.E5.93.81" target="_blank">'. RC_Lang::get('goods::goods.about_goods_preview') .'</a>') . '</p>'
 		);
-		
-		$goods_id = trim($_GET['id']);
+
 		$this->assign('ur_here', RC_Lang::get('goods::goods.goods_preview'));
 		$this->assign('action_linkedit', array('text' => RC_Lang::get('goods::goods.goods_edit'), 'href' => RC_Uri::url('goods/admin/edit', array('goods_id' => $goods_id))));
 		$this->assign('action_link', array('text' => RC_Lang::get('goods::goods.goods_list'), 'href' => RC_Uri::url('goods/admin/init')));
 		
 		$goods = RC_DB::table('goods')->where('goods_id', $goods_id)->orWhere('goods_sn', $goods_id)->first();
-
 		if (empty($goods)) {
 			return $this->showmessage(RC_Lang::get('goods::goods.no_goods'), ecjia::MSGSTAT_ERROR | ecjia::MSGTYPE_HTML, array('links' => array(array('text'=> RC_Lang::get('goods::goods.return_last_page'),'href'=>'javascript:history.go(-1)'))));
 		}
