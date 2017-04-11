d9539314ada9711815575ff6eadca96484ace79e
吴体方 <huangyuyuan@ecmoban.comwhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comthuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comfhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2017-04-11 10:48:18 +0800

    修改查询关联city

--- classes/goods_list.class.php
+++ classes/goods_list.class.php
@@ -179,6 +179,12 @@ class goods_list {
 	        $cache_key .= '-geohash-' . $filter['geohash'];
 		}
 		
+		/* 城市id */
+		if (isset($filter['city_id'])) {
+			$where['sf.city'] = $filter['city_id'];
+			$cache_key .= '-city-' . $filter['city_id'];
+		}
+		
 		if (isset($filter['merchant_cat_id']) && !empty($filter['merchant_cat_id']) && isset($filter['store_id']) && !empty($filter['store_id']) ) {
 		    $merchant_cat_list = RC_DB::table('merchants_category')
 		    	->selectRaw('cat_id')
