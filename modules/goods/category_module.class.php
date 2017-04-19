e705c5af0148908327a3b5c54e552e08cdfb3e32
黄玉园 <huangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2017-04-19 18:32:36 +0800

    更新商品分类

--- modules/goods/category_module.class.php
+++ modules/goods/category_module.class.php
@@ -53,7 +53,7 @@ defined('IN_ECJIA') or exit('No permission resources.');
 class category_module extends api_front implements api_interface {
 	public function handleRequest(\Royalcms\Component\HttpKernel\Request $request) {
 	    RC_Logger::getlogger('info')->info('api_version');
-	    RC_Logger::getlogger('info')->info($request->header('api_version'));
+	    RC_Logger::getlogger('info')->info($request->header());
 	    $api_old = false;
 	    if (version_compare($request->header('api_version'), '1.5', '<')) {
 	        $api_old = true;
