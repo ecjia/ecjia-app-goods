eebead8f0bbc79aa9ebc991991df00241d0a4f2f
黄玉园 <huangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2017-04-24 11:01:05 +0800

    admin/goods/move/category 更新修复

--- modules/admin/goods/move/category_module.class.php
+++ modules/admin/goods/move/category_module.class.php
@@ -50,21 +50,21 @@ defined('IN_ECJIA') or exit('No permission resources.');
  * @author chenzhejun@ecmoban.com
  *
  */
-class category_module implements ecjia_interface
-{
- 	
-    public function run(ecjia_api & $api)
-    {  	
-    	$ecjia = RC_Loader::load_app_class('api_admin', 'api');
-    	$ecjia->authadminSession();
-    	$result = $ecjia->admin_priv('goods_manage');
-    	if (is_ecjia_error($result)) {
-    		EM_Api::outPut($result);
-    	}
+class category_module extends api_admin implements api_interface {
+    public function handleRequest(\Royalcms\Component\HttpKernel\Request $request) {
+
+		$this->authadminSession();
+		if ($_SESSION['admin_id'] <= 0 && $_SESSION['staff_id'] <= 0) {
+			return new ecjia_error(100, 'Invalid session');
+		}
+    	$result = $this->admin_priv('goods_manage');
+        if (is_ecjia_error($result)) {
+			return $result;
+		}
     	
-    	$category_id	= _POST('category_id');//起始分类
-    	$goods_id		= _POST('goods_id');
-    	$target_category_id	= _POST('target_category_id');//目标分类
+    	$category_id	= $this->requestData('category_id');//起始分类
+    	$goods_id		= $this->requestData('goods_id');
+    	$target_category_id	= $this->requestData('target_category_id');//目标分类
     	
     	/* 商品分类不允许为空 */
     	if (empty($category_id) || empty($target_category_id) || empty($goods_id)) {
@@ -74,8 +74,8 @@ class category_module implements ecjia_interface
     	/* 获取商品*/
     	$goods_id = explode(',', $goods_id);
     	$where = array('goods_id' => $goods_id);
-    	if ($_SESSION['ru_id'] > 0) {
-    		$where = array_merge($where, array('user_id' => $_SESSION['ru_id']));
+    	if ($_SESSION['store_id'] > 0) {
+    		$where = array_merge($where, array('store_id' => $_SESSION['store_id']));
     	}
     	$goods_ids = RC_Model::model('goods/goods_model')->where($where)->get_field('goods_id', true);
     	
@@ -85,7 +85,12 @@ class category_module implements ecjia_interface
     	$old_cat_name = RC_Model::model('goods/category_model')->where(array('cat_id' => $category_id))->get_field('cat_name');
     	$query = RC_Model::model('goods/goods_model')->where(array('goods_id' => $goods_id))->update($data);
     	
-    	ecjia_admin::admin_log($old_cat_name.'下商品id为：'.implode(',', $goods_ids).'转移到'.$new_cat_name, 'edit', 'category');
+    	$action = $old_cat_name.'下商品id为：'.implode(',', $goods_ids).'转移到'.$new_cat_name;
+    	if ($_SESSION['store_id'] > 0) {
+    	    RC_Api::api('merchant', 'admin_log', array('text' => $action.'【来源掌柜】', 'action' => 'edit', 'object' => 'category'));
+    	} else {
+    	    ecjia_admin::admin_log($action.'【来源掌柜】', 'edit', 'category'); // 记录日志
+    	}
     	
     	return array();
     	
