fdc74657f77b77d4586a6df9d574369a49d4a7c8
周瑞丽 <huangyuyuan@ecmoban.comzhuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comrhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2018-08-27 17:32:24 +0800

    散装商品暂时提交

--- classes/goods.class.php
+++ classes/goods.class.php
@@ -282,6 +282,7 @@ class goods {
     	$filter ['review_status'] 			= empty ($_REQUEST ['review_status']) 			?  0 	: intval($_REQUEST ['review_status']);
     
     	$where = $filter ['cat_id'] > 0 ? " AND " . merchant_get_children($filter ['cat_id']) : '';
+    	$where .= " AND extension_code !='bulk'";
     
     	/* 推荐类型 */
     	switch ($filter ['intro_type']) {
