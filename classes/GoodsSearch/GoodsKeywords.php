839e9b8ef6459dce74d410d798a2f4404f805481
王正东 <huangyuyuan@ecmoban.comwhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comzhuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comdhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2019-04-02 16:18:11 +0800

    封装商品关键词搜索

---
+++ classes/GoodsSearch/GoodsKeywords.php
@@ -0,0 +1,128 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: royalwang
+ * Date: 2019-04-02
+ * Time: 15:38
+ */
+
+namespace Ecjia\App\Goods\GoodsSearch;
+
+
+class GoodsKeywords
+{
+
+    const WHERE_AND = 'AND';
+
+    const WHERE_OR = 'OR';
+
+    protected $keyword = [];
+
+    protected $operator = self::WHERE_AND;
+
+    /**
+     * GoodsKeywords constructor.
+     * @param $keyword
+     */
+    public function __construct($keyword)
+    {
+        if (is_array($keyword)) {
+            $this->keyword = $keyword;
+        }
+        else {
+            $this->keyword = $this->participle($keyword);
+        }
+
+    }
+
+    /**
+     * 搜索关键词简单分词技术
+     * @param $keyword
+     * @return array
+     */
+    protected function participle($keyword)
+    {
+        if (stristr($keyword, ' AND ') !== false) {
+            /* 检查关键字中是否有AND，如果存在就是并 */
+            $keywords = explode('AND', $keyword);
+            $this->operator = self::WHERE_AND;
+        }
+        elseif (stristr($keyword, ' OR ') !== false) {
+            /* 检查关键字中是否有OR，如果存在就是或 */
+            $keywords = explode('OR', $keyword);
+            $this->operator = self::WHERE_OR;
+        }
+        elseif (stristr($keyword, ' + ') !== false) {
+            /* 检查关键字中是否有加号，如果存在就是或 */
+            $keywords = explode('+', $keyword);
+            $this->operator = self::WHERE_OR;
+        }
+        elseif (stristr($keyword, ' ') !== false) {
+            /* 检查关键字中是否有空格，如果存在就是并 */
+            $keywords = explode(' ', $keyword);
+            $this->operator = self::WHERE_AND;
+        }
+        else {
+            /* 检查关键字中是否有空格，如果存在就是并 */
+            $keywords = (array) $keyword;
+            $this->operator = self::WHERE_AND;
+        }
+
+        return $keywords;
+    }
+
+    public function buildQuery()
+    {
+
+        if ($this->operator == self::WHERE_OR) {
+
+            $query = function ($query) {
+
+                collect($this->keyword)->each(function($item, $key) use ($query) {
+
+                    $subQuery =  function ($query) use ($item) {
+                        $query->where('goods_name', 'like', "%{$item}%")
+                            ->orWhere('goods_sn', 'like', "%{$item}%")
+                            ->orWhere('keywords', 'like', "%{$item}%");
+                    };
+
+                    $query->orWhere($subQuery);
+                });
+
+            };
+
+        }
+        else {
+
+            $query = function ($query) {
+
+                collect($this->keyword)->each(function($item, $key) use ($query) {
+
+                    $subQuery =  function ($query) use ($item) {
+                        $query->where('goods_name', 'like', "%{$item}%")
+                            ->orWhere('goods_sn', 'like', "%{$item}%")
+                            ->orWhere('keywords', 'like', "%{$item}%");
+                    };
+
+                    $query->where($subQuery);
+                });
+
+            };
+        }
+
+        return $query;
+    }
+
+    /**
+     * 保存搜索日志
+     */
+    public function saveSearchLog()
+    {
+
+
+
+
+
+    }
+
+}
