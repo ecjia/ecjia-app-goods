4413707ff6859197ac554f08980baaa7b7afcb01
王正东 <huangyuyuan@ecmoban.comwhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comzhuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comdhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2019-04-03 19:59:41 +0800

    增加新的价格过滤条件

---
+++ classes/GoodsSearch/Filters/Product.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: royalwang
+ * Date: 2019-04-01
+ * Time: 18:24
+ */
+
+namespace Ecjia\App\Goods\GoodsSearch\Filters;
+
+
+use Ecjia\App\Goods\GoodsSearch\FilterInterface;
+use Royalcms\Component\Database\Eloquent\Builder;
+
+class Product implements FilterInterface
+{
+
+    /**
+     * 把过滤条件附加到 builder 的实例上
+     *
+     * @param Builder $builder
+     * @param mixed $value
+     * @return Builder $builder
+     */
+    public static function apply(Builder $builder, $value)
+    {
+        if ($value) {
+            return $builder->leftJoin('products', function ($join) {
+                $join->on('goods.goods_id', '=', 'products.goods_id');
+            })->select('goods.*',
+                'products.goods_id as product_goods_id',
+                'products.goods_attr as product_goods_attr',
+                'products.product_id',
+                'products.product_sn',
+                'products.product_number'
+            );
+        }
+
+        return $builder;
+    }
+
+}
