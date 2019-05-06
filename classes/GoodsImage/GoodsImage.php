9bfd051d5a55a8455ff08e43c040c544c5924bb3
王正东 <huangyuyuan@ecmoban.comwhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comzhuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comdhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2019-05-06 19:03:20 +0800

    封装商品图片上传的存储类

--- classes/GoodsImage/GoodsImage.php
+++ classes/GoodsImage/GoodsImage.php
@@ -68,6 +68,15 @@ class GoodsImage
     }
 
     /**
+     * 获取上传文件的原始文件名
+     * @return string
+     */
+    public function getFileName()
+    {
+        return $this->fileinfo['name'];
+    }
+
+    /**
      * 获取上传后的文件原始路径
      * @return string
      */
@@ -86,8 +95,9 @@ class GoodsImage
         $original_path = $this->disk->getPath($this->image_format->getSourcePostion());
 
         // 生成缩略图
+        $thumb_path = '';
         if ($this->auto_generate_thumb) {
-            $this->saveThumbImageToDisk();
+            $thumb_path = $this->saveThumbImageToDisk();
         }
 
         // 添加水印
@@ -95,42 +105,48 @@ class GoodsImage
 
         // 保存原图
         $this->disk->writeForSourcePath($this->getFilePath(), $original_path);
+
+        //返回 [原图，处理过的图片，缩略图]
+        return [$original_path, $img_path, $thumb_path];
     }
 
     /**
      * 更新图片到数据库
      */
-    public function updateImageToDatabase()
+    public function updateToDatabase($img_desc = null)
     {
+        if (is_null($img_desc)) {
+            $img_desc = $this->getFileName();
+        }
+
+        list($original_path, $img_path, $thumb_path) = $this->saveImageToDisk();
+
+        //存入数据库中
+
+
+
 
     }
 
     /**
      * 保存缩略图到磁盘
+     * 返回缩略图路径
+     * @return string
      */
     public function saveThumbImageToDisk()
     {
-
         $thumb_path = $this->disk->getPath($this->image_format->getThumbPostion());
 
         $this->disk->makeThumb($this->getFilePath(), $thumb_path);
 
+        return $thumb_path;
     }
-
-    /**
-     * 更新缩略图到数据库
-     */
-    public function updateThumbImageToDatabase()
-    {
-
-    }
-
 
     /**
      * 设置是否需要自动生成缩略图，默认为自动生成缩略图
      * @param bool $bool
      */
-    public function set_auto_thumb($bool)
+    public function setAutoGenerateThumb($bool)
     {
         if (is_bool($bool)) {
             $this->auto_generate_thumb = $bool;
---
+++ classes/GoodsImage/MakeGoodsWatermarkImage.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: royalwang
+ * Date: 2019-05-06
+ * Time: 18:36
+ */
+
+namespace Ecjia\App\Goods\GoodsImage;
+
+
+class MakeGoodsWatermarkImage
+{
+
+
+    public function __construct()
+    {
+
+    }
+
+
+}
