5a001d2d1d51330b654ded2d88d8e8e2ec21d963
周瑞丽 <huangyuyuan@ecmoban.comzhuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comrhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2019-05-20 10:21:46 +0800

    货品预览

--- templates/admin/library/goods_store_info.lbi.php
+++ templates/admin/library/goods_store_info.lbi.php
@@ -1,29 +1,43 @@
 <?php defined('IN_ECJIA') or exit('No permission resources.');?>
-<div class="row-fluid preview-merchant-info">
-	<div class="merchant-info">
-		<div class="span5 merchant-info-logo">
-			<div class="shop-logo-thumb">
-		        <img src="{$goods.shop_logo}" >
-		    </div>
-		    <div class="goods-preview-store-info">
-		        {$goods.merchants_name}
-		        <div style="text-align:center;margin-top:15px;"><span class="goods-promote">{t domain='goods'}{$goods.manage_mode}{/t}</span></div>
-		    </div>
-		</div>
-		<div class="separate-line"></div>
-		<div class="span7 merchant-info-name">
-			<div class="store-other-info">
-	        	{t domain="goods"}营业时间：{/t}
-	        	<span class="store-grade">{$goods.shop_trade_time}</span>
-	    	</div>
-	        <div class="store-other-info">
-	        	{t domain="goods"}商家电话：{/t}
-	        	<span class="store-grade">{$goods.shop_kf_mobile}</span>
-	        </div>
-	        <div class="store-other-info">
-	        	{t domain="goods"}商家地址：{/t}
-	        	<span class="store-grade">{$goods.shop_address}</span>
-	        </div>
-		</div>
+<div class="row-fluid">
+	<div class="foldable-list move-mod-group" id="merchants_info_sort_cat">
+	      <div class="accordion-group">
+	           <div class="accordion-heading">
+	               <a class="accordion-toggle collapsed move-mod-head" data-toggle="collapse" data-target="#area_one">
+	                    <strong>{t domain="promotion"}店铺信息{/t}</strong>
+	               </a>
+	           </div>
+	           <div class="accordion-body in in_visable collapse" id="area_one">
+	               <div class="accordion-inner">
+	                   <div class="merchants_info">
+	                       <div class="logo-info">
+	                           <img src="{$goods.shop_logo}" alt="">
+	                           <div class="info">
+	                               <div class="name">{$goods.merchants_name}</div>
+	                               <span class="self-mode">{$goods.manage_mode}</span>
+	                           </div>
+	                       </div>
+	
+	                       <div class="other-info">
+	                           <div class="info-item"><label>营业时间：</label>
+	                               <div class="content">{$goods.shop_trade_time}</div>
+	                           </div>
+	                           <div class="info-item"><label>商家电话：</label>
+	                                <div class="content">{$goods.shop_kf_mobile}</div>
+	                           </div>
+	                           <div class="info-item"><label>商家地址：</label>
+	                                <div class="content">{$goods.shop_address}</div>
+	                           </div>
+	                       </div>
+	                   </div>
+	               </div>
+	          </div>
+	      </div>
 	</div>
 </div>
+ 
+
+            
+            
+
+
