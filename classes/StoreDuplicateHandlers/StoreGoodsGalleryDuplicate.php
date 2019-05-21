fa50087b38d9439badf0f53374c0acd0653dd595
王正东 <huangyuyuan@ecmoban.comwhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comzhuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comdhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2019-05-21 21:02:25 +0800

    主商品数据复制后的后续操作行为抽象

--- classes/StoreDuplicateHandlers/StoreGoodsGalleryDuplicate.php
+++ classes/StoreDuplicateHandlers/StoreGoodsGalleryDuplicate.php
@@ -14,7 +14,7 @@ use ecjia_admin;
  * Class StoreGoodsGalleryDuplicate
  * @package Ecjia\App\Goods\StoreDuplicateHandlers
  */
-class StoreGoodsGalleryDuplicate extends StoreDuplicateAbstract
+class StoreGoodsGalleryDuplicate extends StoreAfterCopyProcessAbstract
 {
     /**
      * 代号标识
@@ -28,48 +28,13 @@ class StoreGoodsGalleryDuplicate extends StoreDuplicateAbstract
      */
     protected $sort = 20;
 
-    protected $dependents = [
-        'store_selling_goods_duplicate',
-        'store_cashier_goods_duplicate'
-    ];
-
-    private $progress_data;
-
-    /**
-     * goods 表中的替换数据
-     * @var array
-     */
-    private $replacement_goods = [];
 
     private $replacement_goods_gallery = [];
-
-    private $table = 'goods_gallery';
 
     public function __construct($store_id, $source_store_id)
     {
         $this->name = __('商品相册', 'goods');
         parent::__construct($store_id, $source_store_id);
-    }
-
-    /**
-     * 获取源店铺数据操作对象
-     */
-    public function getSourceStoreDataHandler()
-    {
-        return RC_DB::table('goods')->where('store_id', $this->source_store_id)->where('is_on_sale', 1)->where('is_delete', '!=', 1);
-    }
-
-    /**
-     * 数据描述及输出显示内容
-     */
-    public function handlePrintData()
-    {
-        $count = $this->handleCount();
-        $text = sprintf(__('店铺内总共有<span class="ecjiafc-red ecjiaf-fs3">%s</span>%s', 'goods'), $count, $this->name);
-
-        return <<<HTML
-<span class="controls-info">{$text}</span>
-HTML;
     }
 
     /**
@@ -87,45 +52,9 @@ HTML;
         // 统计数据条数
         $old_goods_id = $this->getOldGoodsId();
         if (!empty($old_goods_id)) {
-            $this->count = RC_DB::table($this->table)->whereIn('goods_id', $old_goods_id)->count();
+            $this->count = RC_DB::table('goods_gallery')->whereIn('goods_id', $old_goods_id)->count();
         }
         return $this->count;
-    }
-
-    /**
-     * 执行复制操作
-     *
-     * @return mixed
-     */
-    public function handleDuplicate()
-    {
-        //检测当前对象是否已复制完成
-        if ($this->isCheckFinished()) {
-            return true;
-        }
-
-        //如果当前对象复制前仍存在依赖，则需要先复制依赖对象才能继续复制
-        if (!empty($this->dependents)) { //如果设有依赖对象
-            //检测依赖
-            $items = $this->dependentCheck();
-            if (!empty($items)) {
-                return new ecjia_error('handle_duplicate_error', __('复制依赖检测失败！', 'store'), $items);
-            }
-        }
-
-        //执行具体任务
-        $result = $this->startDuplicateProcedure();
-        if (is_ecjia_error($result)) {
-            return $result;
-        }
-
-        //标记处理完成
-        $this->markDuplicateFinished();
-
-        //记录日志
-        $this->handleAdminLog();
-
-        return true;
     }
 
     /**
@@ -196,46 +125,6 @@ HTML;
             }
             //dd($replacement_goods_gallery, $items);
         });
-    }
-
-    /**
-     * 设置 goods 替换数据
-     * @return $this
-     */
-    protected function setReplacementGoodsAfterSetProgressData()
-    {
-        if (empty($this->replacement_goods)) {
-            //获取当前依赖下所有商品替换数据
-            foreach ($this->dependents as $code) {
-                $this->replacement_goods += $this->progress_data->getReplacementDataByCode($code);
-            }
-        }
-        return $this;
-    }
-
-    /**
-     * 设置过程数据
-     * @return $this
-     */
-    protected function setProgressData()
-    {
-        if (empty($this->progress_data)) {
-            //从过程数据中提取需要用到的替换数据
-            $this->progress_data = (new \Ecjia\App\Store\StoreDuplicate\ProgressDataStorage($this->store_id))->getDuplicateProgressData();
-        }
-        return $this;
-    }
-
-    /**
-     * 获取源店铺中的 goods_id
-     * @return array
-     */
-    protected function getOldGoodsId()
-    {
-        if (empty($this->replacement_goods)) {
-            return $this->getSourceStoreDataHandler()->lists('goods_id');
-        }
-        return array_keys($this->replacement_goods);
     }
 
     /**
