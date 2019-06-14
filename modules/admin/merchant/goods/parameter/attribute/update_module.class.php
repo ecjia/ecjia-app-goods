94c8c606264f62591f0c66f2820a6c31b309610e
周瑞丽 <huangyuyuan@ecmoban.comzhuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comrhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2019-06-14 09:19:23 +0800

    编辑参数属性修改

--- modules/admin/merchant/goods/parameter/attribute/update_module.class.php
+++ modules/admin/merchant/goods/parameter/attribute/update_module.class.php
@@ -58,7 +58,7 @@ class admin_merchant_goods_parameter_attribute_update_module extends api_admin i
 			return new ecjia_error(100, 'Invalid session');
 		}
 		
-		$parameter_id 		= intval($attribute_info->goods_type_model->cat_id);
+		$parameter_id 		= intval($this->requestData('parameter_id', 0));
 		$attr_id			= intval($this->requestData('attr_id', 0));
 		$attr_name			= trim($this->requestData('attr_name', ''));
 		$attr_values		= $this->requestData('attr_values', []);
@@ -66,14 +66,11 @@ class admin_merchant_goods_parameter_attribute_update_module extends api_admin i
 		
 		$store_id 			= $_SESSION['store_id'];
 
-		if (empty($attr_id)) {
+		if (empty($attr_id) || empty($parameter_id)) {
 			return new ecjia_error('invalid_parameter', __('参数错误', 'goods'));
 		}
 		
 		$attribute_info = Ecjia\App\Goods\Models\AttributeModel::where('attr_id', $attr_id)->first();
-		if (empty($parameter_id)) {
-			$parameter_id = $attribute_info->cat_id;
-		}
 		
 		$data = [];
 		if (!empty($attr_name)) {
