caab8907a75e5158a08c301c4b5d0df6dda93fde
周瑞丽 <huangyuyuan@ecmoban.comzhuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comrhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2019-06-21 11:32:41 +0800

    货品图片上传调试

--- modules/admin/merchant/goods/product/update_module.class.php
+++ modules/admin/merchant/goods/product/update_module.class.php
@@ -104,7 +104,10 @@ class admin_merchant_goods_product_update_module extends api_admin implements ap
     	
     	//货品图片上传
     	if (isset($_FILES['product_image'])) {
-    		$this->processProductImage($_FILES['product_image'], $goods_id, $product_id);
+    		$upload_result = $this->processProductImage($_FILES['product_image'], $goods_id, $product_id);
+    		if (is_ecjia_error($upload_result)) {
+    			return $upload_result;
+    		}
     	}
     	
     	if (!empty($update_data)) {
@@ -144,6 +147,9 @@ class admin_merchant_goods_product_update_module extends api_admin implements ap
     	if ($proc_goods_img) {
     		if (isset($file_goods_image)) {
     			$image_info = $upload->upload($file_product_image);
+    			if (empty($image_info)) {
+    				return new ecjia_error('upload_error', $upload->error());
+    			}
     		}
     	}
     		 
