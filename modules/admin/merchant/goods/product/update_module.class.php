779e219eb048b55b94577e2a899be366314d6e16
周瑞丽 <huangyuyuan@ecmoban.comzhuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comrhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2019-06-21 13:46:09 +0800

    移除调试

--- modules/admin/merchant/goods/product/update_module.class.php
+++ modules/admin/merchant/goods/product/update_module.class.php
@@ -147,15 +147,6 @@ class admin_merchant_goods_product_update_module extends api_admin implements ap
     			}
     		}
     	}
-    		 
-    	RC_Logger::getlogger('info')->info([
-		    'file' => __FILE__,
-		    'line' => __LINE__,
-		    'product_image' => $file_product_image,
-		    '$_FILES' => $_FILES,
-		    'proc_goods_img' => $proc_goods_img,
-		    'image_info' => $image_info,
-		]);
 
     	/* 更新上传后的商品图片 */
     	if ($proc_goods_img) {
